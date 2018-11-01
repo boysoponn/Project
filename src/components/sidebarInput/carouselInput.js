4419665cb60ee82f2e2a047b7e9b2520852ec596
Boysopon <soponcmm@gmail.com>
2018-11-01 23:00:25 +0700

    gg

--- src/components/sidebarInput/carouselInput.js
+++ src/components/sidebarInput/carouselInput.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import config from '../../config';
-import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
