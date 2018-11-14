dc258a893a35cb5300c109ed57d09e568b6eac63
Sopon Goongit <sopon.g@keenpub.local>
2018-11-14 17:41:18 +0700

    f

--- src/components/template/Menubar/menubarNo1.js
+++ src/components/template/Menubar/menubarNo1.js
@@ -6,34 +6,21 @@ const MenuItemGroup = Menu.ItemGroup;
 
 class Menubar extends React.Component {
   state = {
-    current: 'mail',
-    posts1 : [
-      {id: 2, title: 'fff', content: 'Welcome to learning React!'},
-      {id: 1, title:[{ g:'ddd'},{g:'sds'}], content: 'You can install React from npm.'}
-    ]
   }
-
-//   handleClick = (e) => {
-//     console.log('click ', e);
-//     this.setState({
-//       current: e.key,
-//     });
-//   }
 
   render() {
     return (
       <Menu
       mode="horizontal"
-        // onClick={this.handleClick}
         selectedKeys={[this.state.current]}
       >
       {this.props.menubar.map((post) => 
       post.typeGroup === true ?
         <SubMenu key={post._key} title={<span className="submenu-title-wrapper">{post.label}</span>}>
           <MenuItemGroup>
-          {/* {post.group.map((post) => 
-          <Menu.Item key={post._key}>
-          <a href="https://ant.design"  target="_blank" rel="noopener noreferrer2">{post.content}</a>
+          {/* {post.group.map((pgg) => 
+          <Menu.Item >
+          <a href="https://ant.design"  target="_blank" rel="noopener noreferrer2">{pgg.label}</a>
           </Menu.Item>
           )} */}
           </MenuItemGroup>
