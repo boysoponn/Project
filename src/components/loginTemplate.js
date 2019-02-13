1e31b0fcb6065617bd29b6f56daf941e08cf610e
Sopon Goongit <sopon.g@keenpub.local>
2019-02-13 18:02:31 +0700

    captcha

--- src/components/loginTemplate.js
+++ src/components/loginTemplate.js
@@ -6,6 +6,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import styled from 'styled-components'
 import Social from './template/buttonSocial';
+import ReCAPTCHA from "react-google-recaptcha";
 
 class FormDialog extends React.Component {
   state = {
@@ -14,6 +15,10 @@ class FormDialog extends React.Component {
 
 
   render() {
+    function onChange(value) {
+      console.log("Captcha value:", value);
+    }
+    const recaptchaRef = React.createRef();
     return (
       
         <Dialog
@@ -23,7 +28,11 @@ class FormDialog extends React.Component {
         >
           <DialogTitle  id="form-dialog-title">{this.props.label}</DialogTitle>
           <form onSubmit={this.props.submit}>
-          <div>
+          <ReCAPTCHA
+        ref={recaptchaRef}
+        sitekey="6LdRLpEUAAAAAGX-vss-WN3KL0Xoifa6FFewGxn3"
+        onChange={onChange}
+      />
           <DialogContent style={{paddingTop:0}}>
           {this.props.textField.map((data) => 
             <TextField
@@ -51,7 +60,6 @@ class FormDialog extends React.Component {
           </Content>
             :null}
           </DialogContent>
-          </div>
           </form>
         </Dialog>
         
